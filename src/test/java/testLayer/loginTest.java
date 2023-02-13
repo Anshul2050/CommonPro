3dafa1be08ce4f050e60a7aeae6c44250159d634
anshul <anshuul2050@gmail.com>
2023-02-13 17:47:29 +0530

    2 commit

---
+++ src/test/java/testLayer/loginTest.java
@@ -0,0 +1,18 @@
+package testLayer;
+
+import org.testng.annotations.Test;
+
+public class loginTest {
+	
+	@Test
+	public void ComTest() {
+		System.out.println("Hello");
+	}
+	
+	@Test
+	public void ComTest1() {
+		System.out.println("World");
+		
+	}
+
+}
